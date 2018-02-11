afd677dc5bc1602e23460bd955e18287c7610f68
Andrei Zinovyev <zinovyev@gmail.com>
2018-02-11 03:25:35 +0100

    minor bug in the install.bat

--- install.bat
+++ install.bat
@@ -5,5 +5,5 @@ xcopy bin\fastica++\binaries\doICAInstaller.windows  bin\fastica++\doICA\install
 move bin\fastica++\doICA\installers\doICAInstaller.windows bin\fastica++\doICA\installers\doICAInstaller.exe
 
 xcopy bin\fastica++\binaries\plotAverageStability.windows  bin\fastica++\doICA\installers\* /y
-move bin\fastica++\doICA\installers\plotAverageStability.windows bin\fastica++\doICA\installers\plotAverageStability.exe
+move bin\fastica++\doICA\installers\plotAverageStability.windows bin\fastica++\doICA\plotAverageStability.exe
  
